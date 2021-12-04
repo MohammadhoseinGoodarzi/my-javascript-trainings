8436c6b9b141a6c8dbb73703f119f58ecd855a52
MHG <mhgoodarzi94@gmail.com>
2021-12-04 18:58:54 +0330

    section4

--- main.js
+++ main.js
@@ -7,9 +7,22 @@
 // }
 
 //section3
-document.getElementById("test").innerHTML =
-  "this is third section with document.getElementById()";
-document.write("section3 with document.write()");
-alert("section3 with window.alert() or alert()");
-console.log("section3 with console.log()");
-window.print()
+// document.getElementById("test").innerHTML =
+//   "this is third section with document.getElementById()";
+// document.write("section3 with document.write()");
+// alert("section3 with window.alert() or alert()");
+// console.log("section3 with console.log()");
+// window.print()
+
+//section4 statement
+let c, a, b;
+a = 5;
+b = 95;
+c = b / a;
+console.log(c); //19
+
+const z = 400;
+const y = 600;
+let x = (z * y) / 10000; //24
+console.log(x);
+console.log(x - c); //5
