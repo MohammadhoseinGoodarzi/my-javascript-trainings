6adcbb694ae6ca4b7997afcb484e3d56f6d60bb9
MHG <mhgoodarzi94@gmail.com>
2021-12-04 18:22:18 +0330

    training section2

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+// section1
 // document.getElementById("test").innerHTML = "This is my training ground!";
 
+
+//section2
 function textChange() {
   document.getElementById("test").innerHTML = "chage text in section2";
 }
