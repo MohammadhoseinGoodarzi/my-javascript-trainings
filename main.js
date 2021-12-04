c5437ecf778c174f5f9eb0d585e50e3e13452b81
MHG <mhgoodarzi94@gmail.com>
2021-12-04 18:36:10 +0330

    section3

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 // section1
 // document.getElementById("test").innerHTML = "This is my training ground!";
 
-
 //section2
-function textChange() {
-  document.getElementById("test").innerHTML = "chage text in section2";
-}
+// function textChange() {
+//   document.getElementById("test").innerHTML = "chage text in section2";
+// }
+
+//section3
+document.getElementById("test").innerHTML =
+  "this is third section with document.getElementById()";
+document.write("section3 with document.write()");
+alert("section3 with window.alert() or alert()");
+console.log("section3 with console.log()");
+window.print()
